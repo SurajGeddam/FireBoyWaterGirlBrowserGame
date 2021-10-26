18aec05f9c80537f0ec8ab0ddd1a970968445634
sujay <sujay.jag@gmail.com>
2021-10-26 19:04:02 -0400

    grid

--- src/main.js
+++ src/main.js
@@ -2,31 +2,46 @@ console.log('text')
 
 const canvas = document.getElementById('canvas');
 
-canvas.width = 300;
-canvas.height = 300;
+canvas.width = 420;
+canvas.height = 420;
 
 const ctx = canvas.getContext('2d');
 
 
 console.log('text 2')
+// Box width
+var bw = 400;
+// Box height
+var bh = 400;
+// Padding
+var p = 10;
 
+let y = 20;
 
-let x = 20;
-let y = 30;
+function drawBoard(){
+    for (var x = 0; x <= bw; x += 40) {
+        ctx.moveTo(0.5 + x + p, p);
+        ctx.lineTo(0.5 + x + p, bh + p);
+    }
 
-let d = 2;
-
-let t = 2;
-
+    for (var x = 0; x <= bh; x += 40) {
+        ctx.moveTo(p, 0.5 + x + p);
+        ctx.lineTo(bw + p, 0.5 + x + p);
+    }
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+}
 
 const gameLoop = (timestamp) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
-    ctx.rect(x, 20, 150, 100);
+    ctx.rect(y, 20, 25, 25g);
     ctx.stroke();
 
-    x++; 
+    y++; 
+
+    drawBoard();
 
     requestAnimationFrame(gameLoop);
 }
