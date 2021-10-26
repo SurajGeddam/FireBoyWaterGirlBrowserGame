af2c2b7156a015238b8a01a93aff344edbfb2c7f
Suraj <geddamsuraj@gmail.com>
2021-10-26 19:06:27 -0400

    change 2

--- src/main.js
+++ src/main.js
@@ -7,17 +7,22 @@ canvas.height = 700;
 
 const ctx = canvas.getContext('2d');
 
-
 console.log('text 2')
+
+let x = 20;
 
 const gameLoop = (timestamp) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
-    ctx.rect(x, 20, 150, 100);
+    ctx.rect(x, y, 150, 100);
     ctx.stroke();
 
     x++; 
+
+    if (x%2==0) {
+        y++;
+    }
 
     requestAnimationFrame(gameLoop);
 }
