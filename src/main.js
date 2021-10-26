f7bc3f849577987eb3521cccad14d467ac0fcd07
Suraj <geddamsuraj@gmail.com>
2021-10-26 19:14:48 -0400

    change 5

--- src/main.js
+++ src/main.js
@@ -10,19 +10,36 @@ const ctx = canvas.getContext('2d');
 console.log('text 2')
 
 let x = 20;
+let y = 20;
 
 const gameLoop = (timestamp) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Red rectangle
     ctx.beginPath();
+    ctx.lineWidth = "6";
+    ctx.strokeStyle = "red";
+    ctx.rect(x, 20, 150, 100);
+    ctx.stroke();
+    
+
+    // Green rectangle
+    ctx.beginPath();
+    ctx.lineWidth = "4";
+    ctx.strokeStyle = "green";
+    ctx.rect(20, y, 150, 100);
+    ctx.stroke();
+    y++;
+
+    // Blue rectangle
+    ctx.beginPath();
+    ctx.lineWidth = "10";
+    ctx.strokeStyle = "blue";
     ctx.rect(x, y, 150, 100);
     ctx.stroke();
 
-    x++; 
-
-    if (x%2==0) {
-        y++;
-    }
+    x++;
+    y++;
 
     requestAnimationFrame(gameLoop);
 }
