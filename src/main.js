2a7b0c861ce1e4f55ca675c724a62a3ea9207e45
SurajGeddam <geddamsuraj@gmail.com>
2021-11-03 00:19:41 -0400

    s

--- src/main.js
+++ src/main.js
@@ -19,23 +19,7 @@ const gameLoop = (timestamp) => {
     ctx.beginPath();
     ctx.lineWidth = "6";
     ctx.strokeStyle = "red";
-    ctx.rect(x, 20, 150, 100);
-    ctx.stroke();
-    
-
-    // Green rectangle
-    ctx.beginPath();
-    ctx.lineWidth = "4";
-    ctx.strokeStyle = "green";
-    ctx.rect(20, y, 150, 100);
-    ctx.stroke();
-    y++;
-
-    // Blue rectangle
-    ctx.beginPath();
-    ctx.lineWidth = "10";
-    ctx.strokeStyle = "blue";
-    ctx.rect(x, y, 150, 100);
+    ctx.rect(x, 20, 150, 150);
     ctx.stroke();
 
     x++;
